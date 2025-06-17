34b32534e0f63d16538c7c2c7c870b85bb017e34
godmode99 <bestpratice168@gmail.com>
2025-06-17 23:42:09 +0700

    Create project folder structure

---
+++ ProtocolVisionIV4/camera_manager.py
@@ -0,0 +1,10 @@
+"""Manage multiple camera connections."""
+
+class CameraManager:
+    """Placeholder for camera management logic."""
+
+    def __init__(self):
+        self.cameras = []
+
+    def add_camera(self, camera):
+        self.cameras.append(camera)
---
+++ ProtocolVisionIV4/logger.py
@@ -0,0 +1,8 @@
+"""Logging and export utilities."""
+
+class Logger:
+    """Placeholder for logging implementation."""
+
+    def log(self, message: str) -> None:
+        """Log a message."""
+        print(message)
